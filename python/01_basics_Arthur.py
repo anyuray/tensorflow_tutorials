404700ffe96e826471916901f93cc20ff6adbb6e
v-arcao <taohecaoyu@126.com>
2017-08-02 21:18:33 -0700

    arthur's first test

---
+++ python/01_basics_Arthur.py
@@ -0,0 +1,5 @@
+color = ['red','black','green','blue']
+
+for i in range(len(color)):
+    print('when i was %d, i like %s' %(i,color[i]))
+    # print("He's got %s eyes and %s hair." % (i, color[i]))
